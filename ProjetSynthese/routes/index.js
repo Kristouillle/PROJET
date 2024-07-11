221352fadc0585c952c46498c39b1c7f689c9843
Gaby Langis <156359638+HelixQc@users.noreply.github.com>
2024-07-11 15:03:13 -0400

    Revert "Feature a1"

--- ProjetSynthese/routes/index.js
+++ ProjetSynthese/routes/index.js
@@ -5,35 +5,22 @@ const jwtDecode = require('jsonwebtoken').decode;
 var axios = require('axios')
 var api_url = process.env.API_URL
 
-//moke page need to be replace with a cute page every time you see one!!!!
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/reset_password', function(req,res,next){
-  res.render('moke')
-})
-
 
 router.post('/login', async function(req, res, next) {
 
   const { email, password } = req.body;
+
   try {
     const response = await axios.post(`${api_url}/route/login`, {
       email,
-      password
-    }, {
-      withCredentials: true 
+      password,
     });
-
-    const cookies = response.headers['set-cookie'];
-    if (cookies) {
-      cookies.forEach(cookie => {
-        res.append('Set-Cookie', cookie);
-      });
-    }
 
     const roleClaim = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
     const nameClaim = 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier';
@@ -46,16 +33,6 @@ router.post('/login', async function(req, res, next) {
     console.log('Role:', role);
     console.log('UserName:', name);
     console.log('Decoded Token:', decodedToken);
-
-    const cookieOptions = {
-      httpOnly: true,
-      secure: false, // switch to true when we have a domaine and a certificat
-      sameSite: `strict`,
-      maxAge: 45 // we need to find a balance in time for production
-    }
-
-    res.append('Set-Cookie', `xxx-Oauth=${token}; HttpOnly; Secure; SameSite=Strict; Max-Age=${cookieOptions.maxAge}`);
-    
     /*
     // Redirection based on role
     if (role === 'admin') {
@@ -78,40 +55,5 @@ router.post('/login', async function(req, res, next) {
     res.status(500).send('An error occurred during login.');
   }
 });
-
-router.post('/register', async function(req,res,next){
-  const {firstName, lastName, username,email, passwordHash, phoneNumber} = req.body;
-
-  try{
-     console.log(firstName)
-     console.log(lastName)
-     console.log(username)
-     console.log(email)
-     console.log(passwordHash)
-     console.log(phoneNumber)
-     const response = await axios.post(`${api_url}/user/Register`,
-      {
-        firstName,
-        lastName,
-        username,
-        email,
-        passwordHash,
-        phoneNumber
-      })
-      //Change the button to let know the client is demande is loading
-      res.render('moke')
-  }catch(error){
-    console.error('An error occurred during login:', error);
-    res.status(500).send('Seems like you incurred an error while creating a new user.');
-  }
-
-});
-
-router.post('/logout',async function(req,res,next){
-  //const response = await axios.post(`${api_url}/Route/LogOut`)
-  res.clearCookie('RefreshToken');
-  res.clearCookie('xxx-Oauth');
-  res.redirect('/');
-})
 
 module.exports = router;
