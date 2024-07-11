8e9d9fec6235303b7dd22116b5add7b50721ed5d
HelixQc <signal.gaby@gmail.com>
2024-07-11 14:06:49 -0400

    creating cookie

--- ProjetSynthese/routes/index.js
+++ ProjetSynthese/routes/index.js
@@ -5,21 +5,25 @@ const jwtDecode = require('jsonwebtoken').decode;
 var axios = require('axios')
 var api_url = process.env.API_URL
 
+//moke page need to be replace with a cute page every time you see one!!!!
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/reset_password', function(req,res,next){
+  res.render('moke')
+})
+
 
 router.post('/login', async function(req, res, next) {
 
   const { email, password } = req.body;
-
   try {
     const response = await axios.post(`${api_url}/route/login`, {
       email,
-      password,
+      password
     }, {
       withCredentials: true 
     });
@@ -42,6 +46,16 @@ router.post('/login', async function(req, res, next) {
     console.log('Role:', role);
     console.log('UserName:', name);
     console.log('Decoded Token:', decodedToken);
+
+    const cookieOptions = {
+      httpOnly: true,
+      secure: false, // switch to true when we have a domaine and a certificat
+      sameSite: `strict`,
+      maxAge: 45 // we need to find a balance in time for production
+    }
+
+    res.append('Set-Cookie', `xxx-Oauth=${token}; HttpOnly; Secure; SameSite=Strict; Max-Age=${cookieOptions.maxAge}`);
+    
     /*
     // Redirection based on role
     if (role === 'admin') {
@@ -66,7 +80,38 @@ router.post('/login', async function(req, res, next) {
 });
 
 router.post('/register', async function(req,res,next){
+  const {firstName, lastName, username,email, passwordHash, phoneNumber} = req.body;
+
+  try{
+     console.log(firstName)
+     console.log(lastName)
+     console.log(username)
+     console.log(email)
+     console.log(passwordHash)
+     console.log(phoneNumber)
+     const response = await axios.post(`${api_url}/user/Register`,
+      {
+        firstName,
+        lastName,
+        username,
+        email,
+        passwordHash,
+        phoneNumber
+      })
+      //Change the button to let know the client is demande is loading
+      res.render('moke')
+  }catch(error){
+    console.error('An error occurred during login:', error);
+    res.status(500).send('Seems like you incurred an error while creating a new user.');
+  }
 
 });
+
+router.post('/logout',async function(req,res,next){
+  //const response = await axios.post(`${api_url}/Route/LogOut`)
+  res.clearCookie('RefreshToken');
+  res.clearCookie('xxx-Oauth');
+  res.redirect('/');
+})
 
 module.exports = router;
